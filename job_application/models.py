94c1502f052e989481eb12a45fa5cce0e1b27560
floria914 <floriawu914@gmail.com>
2023-09-12 17:24:06 +0200

    Store the Data in the Database

--- job_application/models.py
+++ job_application/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Form(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
@@ -9,4 +10,5 @@ class Form(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
 
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 
 def index(request):
@@ -12,4 +14,8 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                 email=email, date=date, occupation=occupation)
+
+            messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
